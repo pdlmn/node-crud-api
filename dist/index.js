29f83f4b41d47bd316c3a06a46d298be5f846bf6
pdlmn <djeinwings@gmail.com>
2022-06-19 21:31:20 +0200

    feat: add working server

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = __importDefault(require("http"));
+var port = process.env.PORT || 3000;
+var server = http_1.default.createServer(function (req, res) {
+    console.log('heh');
+});
+server.listen(port, function () { return console.log("Server runs on ".concat(port, " port.")); });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello world!')
+import http from 'http'
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  console.log('heh')
+})
+
+server.listen(port, () => console.log(`Server runs on ${port} port.`))
